f3a661d5160bf210cc42a344af8e69a2012f4145
lukeharby-amiqus <luke@amiqus.co>
2025-06-17 08:02:13 +0000

    php functions

--- index.php
+++ index.php
@@ -1,5 +1,12 @@
 <?php include 'common/header.php';?>
 
-<h1>PHP test</h1>
+    <h1>PHP test</h1>
+
+    <nav>
+        <ul>
+            <li><a href="php-functions.php">php functions</a></li>
+            <li><a href="php-info.php">php info</a></li>
+        </ul>
+    </nav>
 
 <?php include 'common/footer.php'; ?>
