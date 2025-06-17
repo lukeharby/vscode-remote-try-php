a19f5beda3e803bd97b67a9591c078f77470a535
lukeharby-amiqus <luke@amiqus.co>
2025-06-17 07:50:24 +0000

    New shiz

--- common/header.php
+++ common/header.php
@@ -4,5 +4,6 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>php test</title>
+    <link rel="stylesheet" href="/app/css/main.css">
 </head>
 <body>
