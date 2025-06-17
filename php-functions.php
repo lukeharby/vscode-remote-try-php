cda84ec4b562f825c6a16c320255b9bd8063b6f7
lukeharby-amiqus <luke@amiqus.co>
2025-06-17 08:00:32 +0000

    Add functions test

---
+++ php-functions.php
@@ -0,0 +1,7 @@
+<?php include 'common/header.php'; ?>
+
+    <pre>$_ENV: <?php var_dump($_ENV); ?></pre>
+    <pre>getenv(): <?php var_dump(getenv()); ?></pre>
+    <pre>$_SERVER: <?php var_dump($_SERVER); ?></pre>
+
+<?php include 'common/footer.php'; ?>
