d8de6f1939ce898945bf22ac95636db993713f16
lukeharby-amiqus <luke@amiqus.co>
2025-06-17 07:44:19 +0000

    php files

--- index.php
+++ index.php
@@ -1,27 +1,5 @@
-<?php
+<?php include 'common/header.php';?>
 
-/*----------------------------------------------------------------------------------------
- * Copyright (c) Microsoft Corporation. All rights reserved.
- * Licensed under the MIT License. See LICENSE in the project root for license information.
- *---------------------------------------------------------------------------------------*/
+<h1>PHP test</h1>
 
-function sayHello($name) {
-	echo "Hello $name!";
-}
-
-?>
-
-<html>
-	<head>
-		<title>Visual Studio Code Remote :: PHP</title>
-	</head>
-	<body>
-		<?php 
-		
-		sayHello('remote world');
-			
-		phpinfo(); 
-			
-		?>
-	</body>
-</html>
+<?php include 'common/footer.php'; ?>
